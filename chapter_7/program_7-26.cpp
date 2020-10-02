f70566de4afca5b3ace7ba998f7e9360b0523ece
Cameron Moore <cam@cameronlpmoore.com>
2020-10-02 13:56:30 -0700

    adds additional ch_7 exercises

---
+++ chapter_7/program_7-26.cpp
@@ -0,0 +1,26 @@
+// This program demonstrates a range-based for loop with a vector
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // Define and initialize vector
+    vector<int> numbers(5);
+
+    // Get values for the vector elements
+    for (int &val : numbers)
+    {
+        cout << "Enter an integer value: ";
+        cin >> val;
+    }
+
+    // Display the vector elements
+    cout << "Here are the values you entered:\n";
+    for (int val : numbers)
+    {
+        cout << val << endl;
+    }
+
+    return 0;
+}
