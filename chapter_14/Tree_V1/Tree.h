027978caaf57917193281ed697dd52d648a0f231
Cameron Moore <cam@cameronlpmoore.com>
2020-10-27 14:02:26 -0700

    adds ch_14

---
+++ chapter_14/Tree_V1/Tree.h
@@ -0,0 +1,15 @@
+// Tree class
+class Tree
+{
+private:
+    static int objectCount; // Static member variable
+public:
+    // Constructor
+    Tree() { objectCount++; }
+
+    // Accessor function for objectCount
+    int getObjectCount() const { return objectCount; }
+};
+
+// Definition of the static member variable, writtne outside the class.
+int Tree::objectCount = 0;
