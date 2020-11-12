fab573d51baa1d915da82ee0489cfe93952d34a7
Cameron Moore <cam@cameronlpmoore.com>
2020-11-12 12:20:29 -0800

    adds program_14-6.cpp

---
+++ chapter_14/StudentTestScores_V3/program_14-6.cpp
@@ -0,0 +1,27 @@
+// This program demonstrates the overloaded = operator
+#include <iostream>
+#include "StudentTestScores.h"
+using namespace std;
+
+// Function prototype
+void displayStudent(StudentTestScores);
+
+int main()
+{
+    // Create a StudentTestScores object and assign scores
+    StudentTestScores student1("Pancake Lou", 3);
+    student1.setTestScore(100.0, 0);
+    student1.setTestScore(95.0, 1);
+    student1.setTestScore(92.0, 2);
+
+    StudentTestScores student2("Uni Moore", 5);
+
+    // Assign student1 object to student2
+    student2 = student1;
+
+    // Objects will contain the same data
+    displayStudent(student1);
+    displayStudent(student2);
+
+    return 0;
+}
