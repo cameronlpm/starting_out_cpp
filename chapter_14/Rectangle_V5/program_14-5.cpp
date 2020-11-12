c368ff93b2b095acc0feda4f48df0dffe10ec849
Cameron Moore <cam@cameronlpmoore.com>
2020-11-11 23:32:22 -0800

    adds Rectangle_V5

---
+++ chapter_14/Rectangle_V5/program_14-5.cpp
@@ -0,0 +1,24 @@
+// This program demonstrates memberwise assignment.
+#include <iostream>
+#include "Rectangle.h"
+using namespace std;
+
+int main()
+{
+    // Define two Rectangle objects
+    Rectangle box1(10.0, 10.0);
+    Rectangle box2(20.0, 20.0);
+
+    // Display each object's width and length
+    cout << "box1's width and length: " << box1.getWidth() << " " << box1.getLength() << endl;
+    cout << "box2's width and length: " << box2.getWidth() << " " << box2.getLength() << endl;
+
+    // Assign the members of box1 to box2
+    box2 = box1;
+
+    // Display each object's width and length
+    cout << "box1's width and length: " << box1.getWidth() << " " << box1.getLength() << endl;
+    cout << "box2's width and length: " << box2.getWidth() << " " << box2.getLength() << endl;
+
+    return 0;
+}
