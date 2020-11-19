80d9e36bda3e04f541287923b99384fe000520b8
Cameron Moore <cam@cameronlpmoore.com>
2020-11-19 14:45:17 -0800

    chapter_15 initial commit

---
+++ chapter_15/GradedActivity_V1/GradedActivity.h
@@ -0,0 +1,27 @@
+#ifndef GRADEDACTIVITY_H
+#define GRADEDACTIVITY_H
+
+// GradedActivity class declaration
+
+class GradedActivity
+{
+private:
+    double score;
+
+public:
+    // Default constructor
+    GradedActivity() { score = 0.0; }
+
+    // Constructor
+    GradedActivity(double s) { score = s; }
+
+    // Mutator function
+    void setScore(double s) { score = s; }
+
+    // Accessor functions
+    double getScore() const { return score; }
+
+    char getLetterGrade() const;
+};
+
+#endif // GRADEDACTIVITY_H
